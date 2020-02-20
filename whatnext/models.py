ce36dc4a03f5623ff40da9052050335e729cc3b7
Qxf2 <mak@qxf2.com>
2020-02-20 15:06:24 +0530

    Got the top 5 most associated tags with a set of queries

--- whatnext/models.py
+++ whatnext/models.py
@@ -5,9 +5,16 @@ from whatnext import db
 from sqlalchemy import Integer, ForeignKey, String, Column,CheckConstraint
 
 class Raw(db.Model):
-    "Add the raw table" 
+    "Model the raw table" 
     row_id = db.Column(db.String,primary_key=True)   
     answer_count = db.Column(db.String)
     last_activity_date = db.Column(db.String)
     tags = db.Column(db.String)    
     view_count = db.Column(db.String)
+
+class Tagpair(db.Model):
+    "Model the tag pair table"
+    tag1 = db.Column(db.String,primary_key=True)
+    tag2 = db.Column(db.String,primary_key=True)
+    count = db.Column(db.Integer)
+    view_count = db.Column(db.Integer)
--- whatnext/routes.py
+++ whatnext/routes.py
@@ -5,8 +5,7 @@ This file contains all the endpoints for the 'What next' application
 from flask import render_template, url_for, flash, redirect, jsonify, request, Response, session
 from whatnext import app
 from whatnext import db
-
-from whatnext.models import Raw
+from whatnext.models import Raw, Tagpair
 
 @app.route("/",methods=["GET","POST"])
 def home():
@@ -16,4 +15,11 @@ def home():
     if request.method == 'POST':
         query_string = request.form.get('terms')
         technologies = query_string.split(',')
-        return jsonify({'answer':technologies})
+        associated_tech = []
+        for tech in technologies:
+            tech_pair_rows = Tagpair.query.filter(Tagpair.tag1==tech or Tagpair.tag2==tech).order_by(Tagpair.count.desc()).limit(5)
+            for row in tech_pair_rows:
+                associated_tech.append(row.tag1)
+                associated_tech.append(row.tag2)
+        associated_tech = list(set(associated_tech))
+        return jsonify({'answer':associated_tech})
