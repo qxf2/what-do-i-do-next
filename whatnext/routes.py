18986efa548b5d7108b03aeef538828f2049d994
Qxf2 <mak@qxf2.com>
2020-03-05 00:00:41 +0530

    Added a  network graph

--- whatnext/routes.py
+++ whatnext/routes.py
@@ -15,11 +15,17 @@ def home():
     if request.method == 'POST':
         query_string = request.form.get('terms')
         technologies = query_string.split(',')
-        associated_tech = []
+        associated_tech_graph = {'nodes':[],'edges':[]}
+        tech_id = 1
+        edge_id = 100
         for tech in technologies:
+            tech = tech.strip().lower()
             tech_pair_rows = Tagpair.query.filter(Tagpair.tag1==tech or Tagpair.tag2==tech).order_by(Tagpair.count.desc()).limit(5)
             for row in tech_pair_rows:
-                associated_tech.append(row.tag1)
-                associated_tech.append(row.tag2)
-        associated_tech = list(set(associated_tech))
-        return jsonify({'answer':associated_tech})
+                if row.tag1 not in associated_tech_graph['nodes']:
+                    associated_tech_graph['nodes'].append(row.tag1)
+                if row.tag2 not in associated_tech_graph['nodes']:
+                    associated_tech_graph['nodes'].append(row.tag2)
+                if [row.tag1,row.tag2] not in associated_tech_graph['edges']:
+                    associated_tech_graph['edges'].append([row.tag1,row.tag2])
+        return jsonify({'graph':associated_tech_graph})
